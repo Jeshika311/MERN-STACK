9b17cd051003cdcdba11725605b852552585a08f
jeshika311 <jeshikatha@gmail.com>
2025-11-02 13:54:57 +0530

    Lecture-5

---
+++ JavaScript/Lectures/Lecture-5.js
@@ -0,0 +1,25 @@
+// DataTypes
+ 
+// "use strict"; // treat all JS code as newer version
+
+// alert(3+3)  we are using node js, not browser. This syntax is used in browser
+
+let name = "jeshika"
+let age = 18
+let isLoggedIn = false
+
+// number -> 2^53 approx (range)
+// bigint
+// string -> ""  ''
+// boolean -> true/false
+// null -> standalone value
+// undefined -> value is not assigned
+// symbol -> unique
+
+
+// object
+
+console.log(typeof "jeshika")   // string
+console.log(typeof age)    // number
+console.log(typeof undefined)    // undefined
+console.log(typeof null)   // object
