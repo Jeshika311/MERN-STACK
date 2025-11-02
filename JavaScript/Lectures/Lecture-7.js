fdc3ba0ab96856a1200c9d5bdbc8341a07724f7e
jeshika311 <jeshikatha@gmail.com>
2025-11-02 13:55:41 +0530

    Lecture-7

---
+++ JavaScript/Lectures/Lecture-7.js
@@ -0,0 +1,24 @@
+// String to number is confusing
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "helllo"
+let str2 = " hitesh"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2)  // 12
+console.log(1 + "2")  // 12
+console.log("1" + 2 + 2)  // 122
+console.log(1+2+"2")  // 32    
+
+console.log(+true);  // 1
+// console.log(true+);  // error
+
+console.log(+"")  // 0
