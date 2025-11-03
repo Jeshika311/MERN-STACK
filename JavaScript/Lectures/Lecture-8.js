69835f8c3241d3ba583f2426569cc64c30248913
jeshika311 <jeshikatha@gmail.com>
2025-11-03 20:02:58 +0530

    Lecture-8

---
+++ JavaScript/Lectures/Lecture-8.js
@@ -0,0 +1,21 @@
+// comparison of data types
+
+// console.log(2>1)     true
+// console.log(2>=1)    true
+// console.log(2<1)     false
+// console.log(2==1)    false
+// console.log(2!=1)    true
+
+console.log("2">1)    // true
+console.log("02">1)   // true
+
+console.log(null > 0);    // false
+console.log(null == 0);   // false
+console.log(null >= 0);   // true
+
+console.log(undefined > 0);    // false
+console.log(undefined == 0);   // false
+console.log(undefined < 0);   // false
+
+console.log("2" == 2)   // true
+console.log("2" === 2)  // false    === means strict checking. it check data type also
