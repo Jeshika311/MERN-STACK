f911fbba0bd94a93221114e6abe7cb860b0dc6f8
jeshika311 <jeshikatha@gmail.com>
2025-11-19 19:17:54 +0530

    Lecture-25

---
+++ JavaScript/Lectures/Lecture-25.js
@@ -0,0 +1,28 @@
+// HOW DOES JS EXECUTE CODE + CALL STACK
+
+// JS EXECUTION CONTEXT  - > how to run and execute file
+
+// 1.) global execution context bnega hi bnega and isko hum this variable mei rkhege
+
+// this -> window object // browser
+// single threaded
+// -> global execution context
+// -> function execution context
+// -> eval execution context           not impt
+
+// {}  run in two phases
+// -> memory creation phase
+// -> execution phase
+
+// 2.) Memory phase
+// sbse pahle sabhi variables mei undefined aayega
+// this is 1st cycle
+
+// 3.) Execution phase
+// in this value is given to variables 
+
+
+
+// STACK
+
+// LIFO
