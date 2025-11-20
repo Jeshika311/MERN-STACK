4c7389256cf97f318cf31085e0a9813acaa10cc6
jeshika311 <jeshikatha@gmail.com>
2025-11-20 08:56:14 +0530

    Lecture-28

---
+++ JavaScript/Lectures/Lecture-28.js
@@ -0,0 +1,14 @@
+// WHILE AND DO WHILE LOOP
+
+let i = 1
+while (i<=10) {
+    console.log(`Value of index is ${i}`)
+    i++
+}
+
+
+let score = 1
+do {
+    console.log(`Score ${score}`)
+    score++
+} while (score<=10);
