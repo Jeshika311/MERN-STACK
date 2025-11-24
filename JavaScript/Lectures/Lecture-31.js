6de02e1574dfe6d31f71d804a91c0d766a020375
jeshika311 <jeshikatha@gmail.com>
2025-11-24 09:55:37 +0530

    Lecture-31

---
+++ JavaScript/Lectures/Lecture-31.js
@@ -0,0 +1,7 @@
+// DOM INTRODUCTION
+
+// window -> document -> html ->  (head , body)
+
+// head -> (title , meta)   |-> title -> text node     |-> meta -> attribute
+
+// body -> div -> (attribute , h1 ,p)   |-> h1 -> (attribute , text node)    |-> p -> text node
