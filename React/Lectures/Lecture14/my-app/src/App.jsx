51c94fe217b9251f08d25f678c23c0d70ae8850f
jeshika311 <jeshikatha@gmail.com>
2026-02-17 11:06:55 +0530

    Lecture-14

---
+++ React/Lectures/Lecture14/my-app/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState , useMemo } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [input, setInput] = useState(0)
+
+  function expensiveTask(num){
+    console.log("Inside expensive task")
+    for(let i=0; i<=1000000000; i++) {}
+    return num+2;
+  }
+
+  let doubleValue = useMemo(() => expensiveTask(input), [input]);   // memoise only last value 
+
+  return (
+    <div>
+      <button onClick={() => setCount(count+1)}>
+        Increment
+      </button>
+
+      <div>
+        Count : {count}
+      </div>
+
+      <input type='number' 
+      placeholder='Enter number'
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      />
+
+      <div>
+        Count : {doubleValue}
+      </div>
+    </div>
+  )
+}
+
+export default App
